c10854812a673455e591b848d99bac7775dcc1b2
Alexis Morales <amg_077@hotmail.com>
2016-09-05 19:32:11 -0500

    Page created

---
+++ index.php
@@ -0,0 +1,15 @@
+
+
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+  <title>On Time</title>
+</head>
+<body>
+  <div id="big_container">	
+  	<div>	
+      <img id="imagen_reloj" src="fe10.jpg">	
+  	</div> 
+  </div> 	
+</body>	
+</html>
